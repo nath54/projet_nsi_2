32f20183ac6e21c750bd30dc8403cd91b77a998e
nathan cerisara <cerisara.nathan@gmail.com>
2020-12-13 23:43:50 +0100

    tests

---
+++ ultra_note_premium/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=viewport-width, initial-scale=1">
+        <title>UltraNote++ Premium Edition</title>
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Acceuil -->
+        <div id="accueil">
+            <button class="bt_rond" id="bt_left">◄</button>
+            <button class="bt_rond" id="bt_right">►</button>
+            <button class="bt_rond" id="bt_bottom">▼</button>
+        </div>
+        <!-- Connection -->
+
+        <!-- Inscription -->
+        <!-- Infos -->
+    </body>
+</html>
