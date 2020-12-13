23c83e6940e54ccd0cf6cea35caf1c81cefe3ba7
nathan cerisara <cerisara.nathan@gmail.com>
2020-12-13 14:19:45 +0100

    travail sql

--- ultra_note_premium/mariadb/etablissement.sql
+++ ultra_note_premium/mariadb/etablissement.sql
@@ -3,21 +3,36 @@
  Etablissement
 */
 
-INSERT INTO 
+INSERT INTO etablissements (nom, pays, region, ville, adresse, lien_maps, email, phone, academie, membres)
+       VALUES ("Lycée Henri Poincaré", "France", "Grand-Est", "Nancy", "2 Rue de la Visitation, 54000 Nancy", "https://www.google.fr/maps/place/2+Rue+de+la+Visitation,+54000+Nancy/@48.6910198,6.1775567,18z/data=!3m1!4b1!4m5!3m4!1s0x479498727bb0495b:0xb68f2f7a82969331!8m2!3d48.6909808!4d6.1781678",
+               "ce.0540038@ac-nancy-metz.fr", "03 83 17 39 40", "Nancy-Metz", "");
 
 /*
  Professeurs
 */
 
-
-
 /*
  Eleves
 */
 
-
-
 /*
  Classes
 */
+
+INSERT INTO classes (nom, niveau, eleves) VALUES ("2A", "seconde", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("2B", "seconde", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("2C", "seconde", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("2D", "seconde", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("2E", "seconde", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("2F", "seconde", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("1A", "premiere", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("1B", "premiere", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("1C", "premiere", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("1D", "premiere", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("1E", "premiere", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("TA", "terminale", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("TB", "terminale", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("TC", "terminale", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("TD", "terminale", "[]");
+INSERT INTO classes (nom, niveau, eleves) VALUES ("TE", "terminale", "[]");
 
