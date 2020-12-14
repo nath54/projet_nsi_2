b6a778a3847747276328eaaa80dd015a617458f4
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2020-12-14 11:50:23 +0100

    commit

---
+++ ultra_note_premium/main.php
@@ -0,0 +1,22 @@
+<?php
+include("includes/bdd.php");
+?>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=viewport-width, initial-scale=1">
+        <title>UltraNote++ Premium Edition</title>
+        <link href="css/style.css" rel="stylesheet" />
+        <link href="css/main.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- header -->
+        <?php include("includes/header.php"); ?>
+        <!-- Main -->
+        <div>
+            
+        </div>
+        <!-- footer -->
+        <?php include("includes/footer.php"); ?>
+    </body>
+</html>
