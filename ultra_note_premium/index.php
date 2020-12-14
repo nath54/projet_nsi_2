2303a221f9d460f37cc6cf8e3d9f3d06da0955b4
nathan cerisara <cerisara.nathan@gmail.com>
2020-12-14 07:10:45 +0100

    travail.html

--- ultra_note_premium/index.php
+++ ultra_note_premium/index.php
@@ -6,15 +6,38 @@
         <link href="css/style.css" rel="stylesheet" />
     </head>
     <body>
+        <!-- Header -->
+        <div>
+            <div>
+                <h2>UltraNote</h2>
+            </div>
+        </div>
         <!-- Acceuil -->
         <div id="accueil">
-            <button class="bt_rond" id="bt_left">◄</button>
-            <button class="bt_rond" id="bt_right">►</button>
-            <button class="bt_rond" id="bt_bottom">▼</button>
+            <div>
+                <h1>UltraNote++ Premium Edition</h1>
+                <h2>Vivez dans un monde a votre époque</h2>
+                <p>UltraNote est un outil de travail numérique qui vous accompagne tout au long de votre année scolaire, les professeurs autant que les élèves.</p>
+            </div>
+            <div>
+                <h2>(re)venez avec nous !</h2>
+                <div>
+                    <button onclick="tp_connect();">Se connecter</button>
+                    <button onclick="tp_inscription();">S'inscrire</button>
+                </div>
+            </div>
         </div>
         <!-- Connection -->
+        <div id="divconnect">
 
+        </div>
         <!-- Inscription -->
+        <div id="divconnect">
+
+        </div>
         <!-- Infos -->
+        <div id="divinfo">
+
+        </div>
     </body>
 </html>
