ae1da491e7c951ab035b041787da250288aa9d2f
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2020-12-17 19:37:55 +0100

    bonton exit v1

--- ultra_note_premium/index.php
+++ ultra_note_premium/index.php
@@ -30,7 +30,7 @@
         </div>
         <!-- Connection -->
         <div id="divconnect">
-            <button onclick="to_acceuil();">X</button>
+            <button class="bt_disconect" onclick="to_acceuil();">X</button>
             <form id="fconnect" name="fconnect", method="POST", action="includes/connection.php" >
                 <h2>Connection</h2>
                 <div >
@@ -48,7 +48,7 @@
         </div>
         <!-- Inscription -->
         <div id="divinscription">
-            <button onclick="to_acceuil();">X</button>
+            <button class="bt_disconect" onclick="to_acceuil();">X</button>
             <form id="finscription" name="finscription", method="POST", action="includes/inscription.php" >
                 <h2>Enregistrez vous ! </h2>
                 <div >
