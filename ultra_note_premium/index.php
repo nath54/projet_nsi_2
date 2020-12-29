46879dd63e60f4b740619200e90285a1dd9abcca
nathan cerisara <cerisara.nathan@gmail.com>
2020-12-29 23:59:15 +0100

    aaaa

--- ultra_note_premium/index.php
+++ ultra_note_premium/index.php
@@ -49,9 +49,13 @@ echo $txt;
         <meta charset="utf-8" />
         <meta name="viewport" content="width=viewport-width, initial-scale=1">
         <title>UltraNote++ Premium Edition</title>
+        <!--
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+        -->
+        <link href="css/google_fonts/arvo.css" rel="stylesheet" />
+        <link href="css/google_fonts/lato.css" rel="stylesheet" />
         <link href="css/index_dark.css" rel="stylesheet" />
         <link href="css/style_dark.css" rel="stylesheet" />
         <script src="js/lib.js"></script>
--- ultra_note_premium/main.php
+++ ultra_note_premium/main.php
@@ -31,9 +31,13 @@ sessionStorage["actual_page"]="accueil_"+sessionStorage['tp_compte'];
         <meta charset="utf-8" />
         <meta name="viewport" content="width=viewport-width, initial-scale=1">
         <title>UltraNote++ Premium Edition</title>
+        <!--
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+        -->
+        <link href="css/google_fonts/arvo.css" rel="stylesheet" />
+        <link href="css/google_fonts/lato.css" rel="stylesheet" />
         <link href="css/style_dark.css" rel="stylesheet" />
         <link href="css/main.css" rel="stylesheet" />
         <script src="js/lib.js"></script>
