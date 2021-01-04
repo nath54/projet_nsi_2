a441abeeea6e97b18098853a19b1f63795083fec
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-01-04 10:34:49 +0100

    travail

--- ultra_note_premium/includes/pages/eleves_notes.php
+++ ultra_note_premium/includes/pages/eleves_notes.php
@@ -4,7 +4,9 @@ include_once("../init.php");
 include_once("../bdd.php");
 
 $bdd = load_db("../");
-$notes = requete($bdd,"SELECT * FROM eleves_notes WHERE id_eleve = ".$_SESSION["id"]);
+$matieres = requete($bdd,"SELECT * FROM matiere");
+$notes = requete($bdd,"SELECT * FROM notes;");
+$eleves_notes = requete($bdd,"SELECT * FROM eleves_notes WHERE id_eleve = ".$_SESSION["id"]);
 alert($notes);
 
 ?>
