e32787a7e20b3e2d1c809ae2879f4f4a61a2cafb
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-01-04 10:55:37 +0100

    travail

--- ultra_note_premium/includes/pages/eleves_notes.php
+++ ultra_note_premium/includes/pages/eleves_notes.php
@@ -4,12 +4,28 @@ include_once("../init.php");
 include_once("../bdd.php");
 
 $bdd = load_db("../");
-$matieres = requete($bdd,"SELECT * FROM matiere");
-$notes = requete($bdd,"SELECT * FROM notes;");
-$eleves_notes = requete($bdd,"SELECT * FROM eleves_notes WHERE id_eleve = ".$_SESSION["id"]);
-alert($notes);
+$mats = requete($bdd,"SELECT * FROM matieres");
+$notes = requete($bdd,"SELECT * FROM notes");
+
+$matieres=array();
+foreach($mats as $i=>$data){
+    $matieres[$data["id"]]=$data["nom"];
+}
 
 ?>
 
 <div>
+
+<?php
+
+foreach($notes as $i=>$data_note){
+    echo "<table>"
+    foreach(requete($bdd,"SELECT * FROM eleves_notes WHERE id_eleve = ".$_SESSION["id"]." AND id_note = ".$data_note["id"]) as $ii=>$data_eleve){
+        
+    }
+    echo "</table>"
+}
+
+?>
+
 </div>
