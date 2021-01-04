53b943cd1e271939d7b7969bc94fd372ca9f7312
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-01-04 11:24:59 +0100

    cadeau

--- ultra_note_premium/includes/pages/eleves_notes.php
+++ ultra_note_premium/includes/pages/eleves_notes.php
@@ -4,6 +4,7 @@ include_once("../init.php");
 include_once("../bdd.php");
 
 $bdd = load_db("../");
+$id_eleve = $_SESSION["id"];
 $mats = requete($bdd,"SELECT * FROM matieres");
 $notes = requete($bdd,"SELECT * FROM notes");
 
@@ -18,12 +19,16 @@ foreach($mats as $i=>$data){
 
 <?php
 
-foreach($notes as $i=>$data_note){
-    echo "<table>"
-    foreach(requete($bdd,"SELECT * FROM eleves_notes WHERE id_eleve = ".$_SESSION["id"]." AND id_note = ".$data_note["id"]) as $ii=>$data_eleve){
-        
+foreach($matieres as $id_mat=>$nom){
+    $notes = requete($bdd, "SELECT * FROM notes INNER JOIN eleves_notes ON eleves_notes.id_note=notes.id WHERE id_matiere=".$id_mat." AND id_eleve=".$id_eleve);
+    if(count($notes)==0){
+        continue;
     }
-    echo "</table>"
+    echo "<table class='tableau_note'> <tr class='ligne_titre_note'> <td>".$nom."</td> <td>notes</td>  </tr>";
+    foreach($notes as $ii=>$data_note){
+        echo "<tr class='ligne_note'> <td>".$data_note["titre"]."</td> <td>".$data_note["note"]."</td> </tr>";
+    }
+    echo "</table>";
 }
 
 ?>
