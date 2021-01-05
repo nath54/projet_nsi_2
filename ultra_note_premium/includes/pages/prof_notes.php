0edbf18f2c5d8ab387a61f72cde75e9e3afea1c1
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-01-05 10:53:21 +0100

    travail

--- ultra_note_premium/includes/pages/prof_notes.php
+++ ultra_note_premium/includes/pages/prof_notes.php
@@ -23,8 +23,8 @@ foreach($mats as $i=>$data){
 $requete = "SELECT * FROM prof_groupes INNER JOIN groupes ON id_groupe=groupes.id WHERE id_prof=".$id_prof;
 foreach(requete($bdd, $requette) as $i=>$data){
     $data["nom"]
-    echo "<table class='tableau_note'> <tr class='ligne_titre_note'> <td>".$groupe."</td> <td>notes</td>  </tr>";
-
+    echo "<table class='tableau_note'> <tr class='ligne_titre_note'> <td>".$groupe."</td> <td>matiere</td>  </tr>";
+    echo "</table>";
 }
 
 ?>
