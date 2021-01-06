a2d3848b0ebe17e5e4d20c13feae9efc0fe3fc12
nathan cerisara <cerisara.nathan@gmail.com>
2021-01-06 10:30:52 +0100

    travail

--- ultra_note_premium/mariadb/etablissement.sql
+++ ultra_note_premium/mariadb/etablissement.sql
@@ -19,39 +19,39 @@ INSERT INTO etablissements (nom, pays, region, ville, adresse, lien_maps, email,
 
 -- poinca :
 
-INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("aucune", "aucun", 1); --1
-INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TA", "terminale", 1); --2
-INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TB", "terminale", 1); --3
+INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("aucune", "aucun", 1); /*1*/
+INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TA", "terminale", 1); /*2*/
+INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TB", "terminale", 1); /*3*/
 
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("aucun", "aucun", 1); --1
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TA", "terminale", 1); --2
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TB", "terminale", 1); --3
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("aucun", "aucun", 1); /*1*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TA", "terminale", 1); /*2*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TB", "terminale", 1); /*3*/
 
 -- kleber :
 
-INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("aucune", "aucun", 2); --4
-INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TA", "terminale", 2); --5
-INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TB", "terminale", 2); --6
+INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("aucune", "aucun", 2); /*4*/
+INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TA", "terminale", 2); /*5*/
+INSERT INTO classes (nom, niveau, id_etablissement) VALUES ("TB", "terminale", 2); /*6*/
 
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("aucun", "aucun", 2); --4
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TA", "terminale", 2); --5
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TB", "terminale", 2); --6
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("aucun", "aucun", 2); /*4*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TA", "terminale", 2); /*5*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("terminale TB", "terminale", 2); /*6*/
 
 -- GROUPES
 
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("NSI", "terminale", 1); --7
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp1", "terminale", 1); --8
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("Physique-Chimie", "terminale", 1); --9
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp2", "terminale", 1); --10
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("NSI", "terminale", 1); /*7*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp1", "terminale", 1); /*8*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("Physique-Chimie", "terminale", 1); /*9*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp2", "terminale", 1); /*10*/
 
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("NSI", "terminale", 2); --11
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH", "terminale", 2); --12
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("Physique-Chimie", "terminale", 2); --13
-INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp2", "terminale", 1); --14
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("NSI", "terminale", 2); /*11*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH", "terminale", 2); /*12*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("Physique-Chimie", "terminale", 2); /*13*/
+INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp2", "terminale", 1); /*14*/
 
 -- emplois du temps :
 
-INSERT INTO cours SET 
+-- INSERT INTO cours SET id_matiere=
 
 -- emplois du temps :
 
@@ -62,24 +62,25 @@ INSERT INTO cours SET
 
 -- poinca :
 
-INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Torvalds' prenom='Linus' pseudo='linus.torvalds' password_='linustorvalds' naissance='1969-12-28'; --1
-INSERT INTO profs_matieres SET id_prof=1 id_matiere=4;
-INSERT INTO profs_groupes SET id_prof=1 id_groupe=2;
-INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Descartes' prenom='René' pseudo='rene.descartes' password_='renedescartes' naissance='1596-03-31'; --2
-INSERT INTO profs_matieres SET id_prof=2 id_matiere=1;
-INSERT INTO profs_matieres SET id_prof=2 id_matiere=11;
-INSERT INTO profs_groupes SET id_prof=2 id_groupe=2;
-INSERT INTO profs_groupes SET id_prof=2 id_groupe=3;
-INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Poincaré' prenom='Henri' pseudo='henri.poincare' password_='henripoincare' naissance='1854-04-29'; --3
-INSERT INTO profs_matieres SET id_prof=3 id_matiere=1;
-INSERT INTO profs_groupes SET id_prof=2 id_groupe=3;
+INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Torvalds', prenom='Linus', pseudo='linus.torvalds', password_='linustorvalds', naissance='1969-12-28'; /*1*/
+INSERT INTO profs_matieres SET id_prof=1, id_matiere=4;
+INSERT INTO profs_groupes SET id_prof=1, id_groupe=2;
+INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Descartes', prenom='René', pseudo='rene.descartes', password_='renedescartes', naissance='1596-03-31'; /*2*/
+INSERT INTO profs_matieres SET id_prof=2, id_matiere=1;
+INSERT INTO profs_matieres SET id_prof=2, id_matiere=11;
+INSERT INTO profs_groupes SET id_prof=2, id_groupe=2;
+INSERT INTO profs_groupes SET id_prof=2, id_groupe=3;
+INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Poincaré', prenom='Henri', pseudo='henri.poincare', password_='henripoincare', naissance='1854-04-29'; /*3*/
+INSERT INTO profs_matieres SET id_prof=3, id_matiere=1;
+INSERT INTO profs_groupes SET id_prof=2, id_groupe=3;
 
 -- kleber :
 
-INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Enstein' prenom='Albert' pseudo='albert.einstein' password_='alberteinstein' naissance='1879-03-14'; --4INSERT INTO profs_matieres SET id_prof=4 id_matiere=1;
-INSERT INTO profs_matieres SET id_prof=4 id_matiere=5;
-INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Lovelace' prenom='Ada' pseudo='ada.lovelace' password_='adalovelace' naissance='1815-12-10'; --5
-INSERT INTO profs_matieres SET id_prof=5 id_matiere=4;
+INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Enstein', prenom='Albert', pseudo='albert.einstein', password_='alberteinstein', naissance='1879-03-14'; /*4*/
+INSERT INTO profs_matieres SET id_prof=4, id_matiere=1;
+INSERT INTO profs_matieres SET id_prof=4, id_matiere=5;
+INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Lovelace', prenom='Ada', pseudo='ada.lovelace', password_='adalovelace', naissance='1815-12-10'; /*5*/
+INSERT INTO profs_matieres SET id_prof=5, id_matiere=4;
 
 
 /*
@@ -90,12 +91,12 @@ admins :
 
 -- poinca :
 
-INSERT INTO comptes (type_, id_etablissement, pseudo, password_, nom, prenom, naissance) --6
+INSERT INTO comptes (type_, id_etablissement, pseudo, password_, nom, prenom, naissance) /*6*/
        VALUES ("adminhp", 1, "adminhp", MD5("adminhp"), "Admin", "Poinca", "01-01-2001");
 
 -- kleber
 
-INSERT INTO comptes (type_, id_etablissement, pseudo, password_, nom, prenom, naissance) --7
+INSERT INTO comptes (type_, id_etablissement, pseudo, password_, nom, prenom, naissance) /*7*/
        VALUES ("adminkl", 2, "adminkl", MD5("adminkl"), "Admin", "Kleber", "01-01-2001");
 
 /*
@@ -104,31 +105,31 @@ ELEVES
 
 -- poinca
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GHOTN", prenom="Fred", pseudo="fred.ghotn", password_="fredghotn", naissance="2003-05-12"; --8
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GHOTN", prenom="Fred", pseudo="fred.ghotn", password_="fredghotn", naissance="2003-05-12"; /*8*/
 INSERT INTO eleves_classes SET id_eleve=8, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=8, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=8, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=8, id_groupe=8;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="PICHUN", prenom="Michelle", pseudo="michelle.pichun", password_="michellepichun", naissance="2003-02-21"; --9
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="PICHUN", prenom="Michelle", pseudo="michelle.pichun", password_="michellepichun", naissance="2003-02-21"; /*9*/
 INSERT INTO eleves_classes SET id_eleve=9, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=9, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=9, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=9, id_groupe=9;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="CARASE", prenom="Pierre", pseudo="pierre.carase", password_="pierrecarase", naissance="2003-11-09"; --10
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="CARASE", prenom="Pierre", pseudo="pierre.carase", password_="pierrecarase", naissance="2003-11-09"; /*10*/
 INSERT INTO eleves_classes SET id_eleve=10, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=10, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=10, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=10, id_groupe=8;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GODON", prenom="Lilou", pseudo="lilou.godon", password_="lilougodon", naissance="2003-01-01"; --11
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GODON", prenom="Lilou", pseudo="lilou.godon", password_="lilougodon", naissance="2003-01-01"; /*11*/
 INSERT INTO eleves_classes SET id_eleve=11, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=11, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=11, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=11, id_groupe=9;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="DESSA", prenom="Didier", pseudo="didier.dessa", password_="didierdessa", naissance="2003-08-09"; --12
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="DESSA", prenom="Didier", pseudo="didier.dessa", password_="didierdessa", naissance="2003-08-09"; /*12*/
 INSERT INTO eleves_classes SET id_eleve=12, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=12, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=12, id_groupe=7;
