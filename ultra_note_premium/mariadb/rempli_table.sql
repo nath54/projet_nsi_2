6afa6f648a5a77880b84e3a652458f720dabb51f
nathan cerisara <cerisara.nathan@gmail.com>
2021-01-06 10:53:53 +0100

    travail

--- ultra_note_premium/mariadb/rempli_table.sql
+++ ultra_note_premium/mariadb/rempli_table.sql
@@ -92,24 +92,24 @@ INSERT INTO groupes (nom, niveau, id_etablissement) VALUES ("MATH grp2", "termin
 
 -- poinca :
 
-INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Torvalds', prenom='Linus', pseudo='linus.torvalds', password_='linustorvalds', naissance='1969-12-28'; /*1*/
+INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Torvalds', prenom='Linus', pseudo='linus.torvalds', password_=MD5('linustorvalds'), naissance='1969-12-28'; /*1*/
 INSERT INTO profs_matieres SET id_prof=1, id_matiere=4;
 INSERT INTO profs_groupes SET id_prof=1, id_groupe=2;
-INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Descartes', prenom='René', pseudo='rene.descartes', password_='renedescartes', naissance='1596-03-31'; /*2*/
+INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Descartes', prenom='René', pseudo='rene.descartes', password_=MD5('renedescartes'), naissance='1596-03-31'; /*2*/
 INSERT INTO profs_matieres SET id_prof=2, id_matiere=1;
 INSERT INTO profs_matieres SET id_prof=2, id_matiere=11;
 INSERT INTO profs_groupes SET id_prof=2, id_groupe=2;
 INSERT INTO profs_groupes SET id_prof=2, id_groupe=3;
-INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Poincaré', prenom='Henri', pseudo='henri.poincare', password_='henripoincare', naissance='1854-04-29'; /*3*/
+INSERT INTO comptes SET type_='prof', id_etablissement=1, nom='Poincaré', prenom='Henri', pseudo='henri.poincare', password_=MD5('henripoincare'), naissance='1854-04-29'; /*3*/
 INSERT INTO profs_matieres SET id_prof=3, id_matiere=1;
 INSERT INTO profs_groupes SET id_prof=3, id_groupe=3;
 
 -- kleber :
 
-INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Enstein', prenom='Albert', pseudo='albert.einstein', password_='alberteinstein', naissance='1879-03-14'; /*4*/
+INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Enstein', prenom='Albert', pseudo='albert.einstein', password_=MD5('alberteinstein'), naissance='1879-03-14'; /*4*/
 INSERT INTO profs_matieres SET id_prof=4, id_matiere=1;
 INSERT INTO profs_matieres SET id_prof=4, id_matiere=5;
-INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Lovelace', prenom='Ada', pseudo='ada.lovelace', password_='adalovelace', naissance='1815-12-10'; /*5*/
+INSERT INTO comptes SET type_='prof', id_etablissement=2, nom='Lovelace', prenom='Ada', pseudo='ada.lovelace', password_=MD5('adalovelace'), naissance='1815-12-10'; /*5*/
 INSERT INTO profs_matieres SET id_prof=5, id_matiere=4;
 
 
@@ -135,31 +135,31 @@ ELEVES
 
 -- poinca
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GHOTN", prenom="Fred", pseudo="fred.ghotn", password_="fredghotn", naissance="2003-05-12"; /*8*/
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GHOTN", prenom="Fred", pseudo="fred.ghotn", password_=MD5("fredghotn"), naissance="2003-05-12"; /*8*/
 INSERT INTO eleves_classes SET id_eleve=8, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=8, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=8, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=8, id_groupe=8;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="PICHUN", prenom="Michelle", pseudo="michelle.pichun", password_="michellepichun", naissance="2003-02-21"; /*9*/
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="PICHUN", prenom="Michelle", pseudo="michelle.pichun", password_=MD5("michellepichun"), naissance="2003-02-21"; /*9*/
 INSERT INTO eleves_classes SET id_eleve=9, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=9, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=9, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=9, id_groupe=9;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="CARASE", prenom="Pierre", pseudo="pierre.carase", password_="pierrecarase", naissance="2003-11-09"; /*10*/
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="CARASE", prenom="Pierre", pseudo="pierre.carase", password_=MD5("pierrecarase"), naissance="2003-11-09"; /*10*/
 INSERT INTO eleves_classes SET id_eleve=10, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=10, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=10, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=10, id_groupe=8;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GODON", prenom="Lilou", pseudo="lilou.godon", password_="lilougodon", naissance="2003-01-01"; /*11*/
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="GODON", prenom="Lilou", pseudo="lilou.godon", password_=MD5("lilougodon"), naissance="2003-01-01"; /*11*/
 INSERT INTO eleves_classes SET id_eleve=11, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=11, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=11, id_groupe=7;
 INSERT INTO eleves_groupes SET id_eleve=11, id_groupe=9;
 
-INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="DESSA", prenom="Didier", pseudo="didier.dessa", password_="didierdessa", naissance="2003-08-09"; /*12*/
+INSERT INTO comptes SET type_="eleve", id_etablissement=1, nom="DESSA", prenom="Didier", pseudo="didier.dessa", password_=MD5("didierdessa"), naissance="2003-08-09"; /*12*/
 INSERT INTO eleves_classes SET id_eleve=12, id_classe=2;
 INSERT INTO eleves_groupes SET id_eleve=12, id_groupe=2;
 INSERT INTO eleves_groupes SET id_eleve=12, id_groupe=7;
